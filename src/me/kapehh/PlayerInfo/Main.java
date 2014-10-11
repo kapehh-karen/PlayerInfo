b92799cc710d06f08ef8e1c6339c3fb6c0b252b3
kapehh <git@kapehh.net>
2014-10-11 15:26:59 +0400

    second
    $waaaas

--- src/me/kapehh/PlayerInfo/Main.java
+++ src/me/kapehh/PlayerInfo/Main.java
@@ -3,12 +3,30 @@ package me.kapehh.PlayerInfo;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Karen on 11.10.2014.
  */
 public class Main extends JavaPlugin implements CommandExecutor {
+    String format = "=*======================\n" +
+                    "[{name}]\n" +
+                    "\n" +
+                    "Звание: {rank}\n" +
+                    "Здоровье: {hp}\n" +
+                    "\n" +
+                    "Динарии: {money}\n" +
+                    "\n" +
+                    "Убито мобов: {mobs}\n" +
+                    "Убито игроков: {players}\n" +
+                    "Смертей: {deaths}\n" +
+                    "\n" +
+                    "=*======================\n";
+
+    private String processFormat(Player player) {
+        return format.replace("{name}", player.getName());
+    }
 
     @Override
     public void onEnable() {
@@ -17,7 +35,12 @@ public class Main extends JavaPlugin implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Mimimi");
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+        player.sendMessage(processFormat(player));
         return true;
     }
 
